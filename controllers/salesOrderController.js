ccfd470cf726b1e7afcf24f9d20dc3519e2b8079
JanartJ <janarthanan.dstar@gmail.com>
2024-02-14 00:32:29 +0530

    update so api done

--- controllers/salesOrderController.js
+++ controllers/salesOrderController.js
@@ -12,7 +12,7 @@ const getSOList = async (req, res) => {
             order_id:1,
             items:1,
             status:1,
-            _id: 0 // Excluding _id field
+            _id: 1
           }
       }
   ]).then(result => {
@@ -20,6 +20,7 @@ const getSOList = async (req, res) => {
     res.status(200).json({ message: "SO fetched successfully", result});
   }).catch(err => {
       console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
   });  
   } catch (error) {
     console.error(error);
@@ -27,6 +28,20 @@ const getSOList = async (req, res) => {
   }
 };
 
+const editSODetails = async(req,res)=>{ 
+  const filter = { status: "Pending", order_id: req.body.id }; // Update YOUR_ORDER_ID with the specific order ID
+  const update = { status: req.body.status };
+  SalesOrder.updateOne(filter, { $set: update })
+  .then(result => {
+        console.log(result);
+        res.status(200).json({ message: "SO updated successfully", result});
+  }).catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+  });
+}
+
 module.exports = {
-  getSOList
+  getSOList,
+  editSODetails
 };
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const projectController = require("../controllers/projectController");
-
+const SOController = require("../controllers/salesOrderController");
 router.post("/project/create", projectController.createProject);
 router.get("/project/getall", projectController.getAllProjects);
 router.get("/project/getallproductlist", projectController.getAllProjectsList);
+router.get("/so/getsolist", SOController.getSOList);
+router.post("/so/updatesostatus", SOController.editSODetails);
 
 module.exports = router;
 
