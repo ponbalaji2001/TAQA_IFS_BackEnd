231b46a433711402adde987b4aeae3e50fb0dbdb
JanartJ <janarthanan.dstar@gmail.com>
2024-02-13 22:40:37 +0530

    getproductlist api done

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -35,7 +35,35 @@ const getAllProjects = async (req, res) => {
   }
 };
 
+// Get all projects data
+const getAllProjectsList = async (req, res) => {
+  try {
+    // const allProjects = await Project.find();
+    Project.aggregate([
+      {
+          $project: {
+              title: 1,
+              assignee: 1,
+              status: 1,
+              start_date: 1,
+              end_date: 1,
+              _id: 0 // Excluding _id field
+          }
+      }
+  ]).then(result => {
+    console.log("fetched result",result.length);
+    res.status(200).json(result);
+  }).catch(err => {
+      console.error(err);
+  });  
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   createProject,
-  getAllProjects
+  getAllProjects,
+  getAllProjectsList
 };
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -4,5 +4,7 @@ const projectController = require("../controllers/projectController");
 
 router.post("/project/create", projectController.createProject);
 router.get("/project/getall", projectController.getAllProjects);
+router.get("/project/getallproductlist", projectController.getAllProjectsList);
 
 module.exports = router;
+
