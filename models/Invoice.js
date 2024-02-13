fa084326470775585688fc9d61849795b2441a5a
Ponbalaji S <85952783+ponbalaji2001@users.noreply.github.com>
2024-02-14 00:52:15 +0530

    invoice

---
+++ models/Invoice.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const invoiceSchema = new mongoose.Schema({
+    issued_date: {
+      type: Date,
+    },
+    due_date: {
+      type: Date,
+      
+    },
+    billed_to: {
+      type: String,
+     
+    },
+    from: {
+      type: String,
+      
+    },
+    status: {
+        type: String,
+    },    
+    service:[{
+
+    }],
+    sub_total: {
+        type: Number,
+        required: true,
+    },
+    tax: {
+          type: Number,
+          required: true,
+    }, 
+    total: {
+        type: Number,
+        required: true,
+    },
+    amount_due: {
+        type: Number,
+        required: true,
+    },        
+
+});
+  
+const Invoice = mongoose.model("invoice", invoiceSchema);
+module.exports =  Invoice;
