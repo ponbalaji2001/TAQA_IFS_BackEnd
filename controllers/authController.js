10b34ab10137f1d789fd7bbae82e48d877081799
JanartJ <janarthanan.dstar@gmail.com>
2024-02-14 08:47:04 +0530

    auth api updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,25 @@
 const User = require("../models/User");
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
+  console.log( req.body);
+    const { name, password,role } = req.body;
     try {
-      const user = await User.findOne({ username });
+      const user = await User.findOne({ name });
       if (!user) {
         return res.status(401).json({ message: "Invalid user" });
       }
-      console.log(username)
-      console.log(password+"  "+user.password)
-      if (password !== user.password) {
-        return res.status(401).json({ message: "Invalid password" });
+      console.log(user);
+      if(password === user.password && role===user.role){
+        user["password"] = "";
+        res.status(200).json({
+          message: "Authenticated successfully",
+          user: user,
+        });
       }
-      res.status(200).json({
-        message: "Login successful",
-        user: user,
-      });
+      else{
+        return res.status(401).json({ message: "Invalid Credential" });
+      }
+      
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
