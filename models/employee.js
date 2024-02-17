04afaa261e8b451f2d27fd7fbc77e73456d10132
Ponbalaji S <85952783+ponbalaji2001@users.noreply.github.com>
2024-02-17 11:11:32 +0530

    employee crud

--- models/employee.js
+++ models/employee.js
@@ -3,19 +3,32 @@ const mongoose = require("mongoose");
 const employeeMaster = new mongoose.Schema({
     empid: {
       type: String,
+      required:true,
     },
     empname: {
         type: String,
+        required:true,
     },
     designation: {
         type: String,
+        required:true,
     },
     experience: {
         type: String,
+        required:true,
     },
     salary: {
         type: Number,
-    },    
+        required:true,
+    },
+    projects:[{
+        project_id: {
+            type: Number,
+        },
+        project_location: {
+            type: String,
+        },
+    }]    
   });
   
   const EmployeeMaster = mongoose.model("employee", employeeMaster);
