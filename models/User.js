38b0e0c5a7357d1be48ea9ccafbe25f216915c0d
Ponbalaji S <85952783+ponbalaji2001@users.noreply.github.com>
2024-02-19 20:01:33 +0530

    phases

--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+   mapid: {
+     type: mongoose.Schema.Types.ObjectId,
+
+    },
     name: {
       type: String,
       required: true,
